40536d3002cbf4b71548c4e9c58f3431a40efbd4
Nguyen Van Nhat Anh <jemkeveckcoss@gmail.com>
2026-02-13 17:39:28 +0700

    KAKKA

--- DSA_001.cpp
+++ DSA_001.cpp
@@ -1,9 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() {
+int n;
+int x[1005];
+bool ktr() {
+    int l=1,r=n;
+    while(l<r) {
+        if(x[l]!= x[r]) return false;
+        l++;r--;
+    }
+    return true;
+}
 
-    cout << "HAHAHA\n";
+void Try(int pos) {
+    if(pos > n) {
+        if(ktr()) {
+            for(int k=1;k<=n;k++) cout << x[k] << ' ';
+            cout << '\n';
+        }
+        return;
+    }
+    for(int j=0;j<=1;j++) {
+        x[pos] = j;
+        Try(pos+1);
+    }
+}
+
+
+int main() {
+    cin >> n;
+    Try(1);
+
 
 }
