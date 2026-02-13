d55d7862e6714e96fd97df21b5264da382521f31
Nguyen Van Nhat Anh <jemkeveckcoss@gmail.com>
2026-02-13 17:47:10 +0700

    5:46 - 13-Feb-2026

--- DSA_001.cpp
+++ DSA_001.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int n;
-int x[1005];
-bool ktr() {
-    int l=1,r=n;
-    while(l<r) {
-        if(x[l]!= x[r]) return false;
-        l++;r--;
+string s;
+
+bool rev() {
+    int l=0, r= s.length()-1;
+    while(l < r) {
+        if(s[l] != s[r]) return false;
+        ++l;--r;
     }
     return true;
 }
 
-void Try(int pos) {
-    if(pos > n) {
-        if(ktr()) {
-            for(int k=1;k<=n;k++) cout << x[k] << ' ';
-            cout << '\n';
+void Try(int i) {
+    if(i >= n) {
+        if(rev()) {
+            for(int j=0; j < n;j++) cout << s[j] << ' ';
         }
         return;
     }
-    for(int j=0;j<=1;j++) {
-        x[pos] = j;
-        Try(pos+1);
+    for(int j='0';j <= '1';j++) {
+        s[i] = j;
+        Try(i+1);
     }
 }
 
-
 int main() {
-    cin >> n;
-    Try(1);
 
+    cin >> n;
+    s = string(n,'0');
+    Try(0);
 
 }
